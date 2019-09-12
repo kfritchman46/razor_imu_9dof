a37a35cd8eb70a8745b0eb2cb573eb98a71de690
Kyle Fritchman <kfritchman46@gmail.com>
2019-09-11 17:12:51 -0700

    Update MPU9250_DMP.ino

--- Arduino/MPU9250_DMP/MPU9250_DMP.ino
+++ Arduino/MPU9250_DMP/MPU9250_DMP.ino
@@ -1,19 +1,4 @@
 /************************************************************
-MPU9250_DMP_Quaternion
- Quaternion example for MPU-9250 DMP Arduino Library 
-Jim Lindblom @ SparkFun Electronics
-original creation date: November 23, 2016
-https://github.com/sparkfun/SparkFun_MPU9250_DMP_Arduino_Library
-
-The MPU-9250's digital motion processor (DMP) can calculate
-four unit quaternions, which can be used to represent the
-rotation of an object.
-
-This exmaple demonstrates how to configure the DMP to 
-calculate quaternions, and prints them out to the serial
-monitor. It also calculates pitch, roll, and yaw from those
-values.
-
 Development environment specifics:
 Arduino IDE 1.6.12
 SparkFun 9DoF Razor IMU M0
@@ -136,13 +121,13 @@ void loop()
 
 void write_float_bytes(String label, float* data, int _len)
 {
-  SerialPort.print(label+"|f"+(char) sizeof(float)+(char) _len);
+  SerialPort.print(label+"|float"+(char) sizeof(float)+(char) _len);
   SerialPort.write((byte *) data, sizeof(float)*_len);
 }
 
 void write_ulong_bytes(String label, long unsigned int* data, int _len)
 {
-  SerialPort.print(label+"|l"+(char) sizeof(data[0])+(char) _len);
+  SerialPort.print(label+"|uint"+(char) sizeof(data[0])+(char) _len);
   SerialPort.write((byte *) data, sizeof(data[0])*_len);
 }
 
